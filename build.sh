a8fc5066529918f4fd438edcbc0597300652bb48
JingBuzz <119687768+Jeinse@users.noreply.github.com>
2025-08-10 03:47:11 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ KERNEL_DEFCONFIG=vendor/sm8250_defconfig
 DIR=$PWD
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH="/home/wenxuan/clang/clang3399163b/bin"
+export CLANG_PATH="~/toolchains/bin"
 export PATH="$CLANG_PATH:$PATH"
-export CROSS_COMPILE=/home/wenxuan/gcc/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
-export CROSS_COMPILE_ARM32=/home/wenxuan/gcc/gcc-arm-11.2-2022.02-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-
+export CROSS_COMPILE=aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 export KBUILD_BUILD_USER=Amog
 export KBUILD_BUILD_HOST=Us
 
@@ -19,6 +19,6 @@ echo
 echo "Kernel is going to be built using $KERNEL_DEFCONFIG"
 echo
 
-make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LLVM=1 O=out $KERNEL_DEFCONFIG
+make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LLVM=1 LLVM_IAS=1 O=out $KERNEL_DEFCONFIG
 
-make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LLVM=1 O=out -j$(nproc --all)
+make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LLVM=1 LLVM_IAS=1 O=out -j$(nproc --all)
